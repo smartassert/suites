4cfccda5d458f0741621057baf568457fe73aad8
Jon Cram <webignition@gmail.com>
2022-03-30 12:38:39 +0100

    foo Squash create model

--- src/Request/SuiteRequest.php
+++ src/Request/SuiteRequest.php
@@ -4,11 +4,16 @@ namespace App\Request;
 
 class SuiteRequest
 {
+    public const KEY_ID = 'id';
+    public const KEY_SOURCE_ID = 'source_id';
+    public const KEY_LABEL = 'label';
+    public const KEY_TESTS = 'tests';
+
     /**
      * @param array<int, string> $tests
      */
     public function __construct(
-        public readonly string $id,
+        public readonly ?string $id,
         public readonly string $userId,
         public readonly string $sourceId,
         public readonly string $label,
