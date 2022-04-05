7ec9cb3ab571f76868f72713ceafc3c672f1a10d
Jon Cram <webignition@gmail.com>
2022-04-05 18:02:05 +0100

    Factor out create/update controller common logic (#42)

--- src/Controller/SuiteController.php
+++ src/Controller/SuiteController.php
@@ -16,67 +16,30 @@ use Symfony\Component\Uid\Ulid;
 
 class SuiteController extends AbstractController
 {
+    public function __construct(
+        private YamlFilenameValidator $yamlFilenameValidator,
+        private SuiteRepository $repository,
+    ) {
+    }
+
     #[Route('/', name: 'create', methods: ['POST'])]
-    public function create(
-        SuiteRequest $request,
-        YamlFilenameValidator $yamlFilenameValidator,
-        SuiteRepository $repository
-    ): JsonResponse {
+    public function create(SuiteRequest $request): JsonResponse
+    {
         // @todo: replace with injected user's id in #10
         $userId = EntityId::create();
+        $suite = new Suite($userId, EntityId::create(), '');
 
-        $sourceId = $request->sourceId;
-        if (null === $sourceId) {
-            return new ErrorResponse('source_id/missing');
-        }
-
-        if (false === Ulid::isValid($sourceId)) {
-            return new ErrorResponse('source_id/invalid');
-        }
-
-        $label = $request->label;
-        if (null === $label) {
-            return new ErrorResponse('label/missing');
-        }
-
-        $requestTests = $request->tests;
-        $tests = [];
-
-        if (is_array($requestTests)) {
-            $invalidTests = [];
-
-            foreach ($requestTests as $requestTest) {
-                $requestTest = trim($requestTest);
-
-                $validation = $yamlFilenameValidator->validate(Filename::parse($requestTest));
-                if ($validation->isValid()) {
-                    $tests[] = $requestTest;
-                } else {
-                    $invalidTests[] = $requestTest;
-                }
-            }
-
-            if ([] !== $invalidTests) {
-                return new ErrorResponse('tests/invalid', [
-                    'invalid_paths' => $invalidTests,
-                ]);
-            }
-        }
-
-        $suite = new Suite($userId, $sourceId, $label, $tests);
-
-        $repository->add($suite);
-
-        return new JsonResponse($suite);
+        return $this->setSuite($suite, $request);
     }
 
     #[Route(SuiteRoutes::ROUTE_SUITE, name: 'update', methods: ['PUT'])]
-    public function update(
-        Suite $suite,
-        SuiteRequest $request,
-        YamlFilenameValidator $yamlFilenameValidator,
-        SuiteRepository $repository
-    ): JsonResponse {
+    public function update(Suite $suite, SuiteRequest $request): JsonResponse
+    {
+        return $this->setSuite($suite, $request);
+    }
+
+    private function setSuite(Suite $suite, SuiteRequest $request): JsonResponse
+    {
         $sourceId = $request->sourceId;
         if (null === $sourceId) {
             return new ErrorResponse('source_id/missing');
@@ -100,7 +63,7 @@ class SuiteController extends AbstractController
             foreach ($requestTests as $requestTest) {
                 $requestTest = trim($requestTest);
 
-                $validation = $yamlFilenameValidator->validate(Filename::parse($requestTest));
+                $validation = $this->yamlFilenameValidator->validate(Filename::parse($requestTest));
                 if ($validation->isValid()) {
                     $tests[] = $requestTest;
                 } else {
@@ -119,7 +82,7 @@ class SuiteController extends AbstractController
         $suite->setLabel($label);
         $suite->setTests($tests);
 
-        $repository->add($suite);
+        $this->repository->add($suite);
 
         return new JsonResponse($suite);
     }
