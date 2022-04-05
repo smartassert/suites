1af8449afe239721976fb229346c17b5ea096377
Jon Cram <webignition@gmail.com>
2022-04-05 15:12:13 +0100

    Resolve controller return type issue following main rebase

--- src/Controller/SuiteController.php
+++ src/Controller/SuiteController.php
@@ -11,6 +11,7 @@ use SmartAssert\YamlFile\Filename;
 use SmartAssert\YamlFile\Validator\YamlFilenameValidator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Uid\Ulid;
 
