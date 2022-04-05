7c46b74ddbd3b190193b0bdb257c826beaeff363
Jon Cram <webignition@gmail.com>
2022-04-05 17:52:40 +0100

    Factor out SuiteController::create() and ::update() bodies

--- src/Controller/SuiteController.php
+++ src/Controller/SuiteController.php
@@ -24,54 +24,22 @@ class SuiteController extends AbstractController
     ): JsonResponse {
         // @todo: replace with injected user's id in #10
         $userId = EntityId::create();
+        $suite = new Suite($userId, EntityId::create(), '');
 
-        $sourceId = $request->sourceId;
-        if (null === $sourceId) {
-            return new ErrorResponse('source_id/missing');
-        }
-
-        if (false === Ulid::isValid($sourceId)) {
-            return new ErrorResponse('source_id/invalid');
-        }
-
-        $label = $request->label;
-        if (null === $label) {
-            return new ErrorResponse('label/missing');
-        }
-
-        $requestTests = $request->tests;
-        $tests = [];
-
-        if (is_array($requestTests)) {
-            $invalidTests = [];
-
-            foreach ($requestTests as $requestTest) {
-                $requestTest = trim($requestTest);
-
-                $validation = $yamlFilenameValidator->validate(Filename::parse($requestTest));
-                if ($validation->isValid()) {
-                    $tests[] = $requestTest;
-                } else {
-                    $invalidTests[] = $requestTest;
-                }
-            }
-
-            if ([] !== $invalidTests) {
-                return new ErrorResponse('tests/invalid', [
-                    'invalid_paths' => $invalidTests,
-                ]);
-            }
-        }
-
-        $suite = new Suite($userId, $sourceId, $label, $tests);
-
-        $repository->add($suite);
-
-        return new JsonResponse($suite);
+        return $this->setSuite($suite, $request, $yamlFilenameValidator, $repository);
     }
 
     #[Route(SuiteRoutes::ROUTE_SUITE, name: 'update', methods: ['PUT'])]
     public function update(
+        Suite $suite,
+        SuiteRequest $request,
+        YamlFilenameValidator $yamlFilenameValidator,
+        SuiteRepository $repository
+    ): JsonResponse {
+        return $this->setSuite($suite, $request, $yamlFilenameValidator, $repository);
+    }
+
+    private function setSuite(
         Suite $suite,
         SuiteRequest $request,
         YamlFilenameValidator $yamlFilenameValidator,
