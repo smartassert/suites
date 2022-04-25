eae1d2cd6e12f741e0a3c8ba88eb90a92eecc6fe
Jon Cram <webignition@gmail.com>
2022-04-25 15:36:38 +0100

    Add remaining integration tests

---
+++ tests/Integration/InvalidSuiteUserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use App\Tests\Application\AbstractInvalidSuiteUserTest;
+
+class InvalidSuiteUserTest extends AbstractInvalidSuiteUserTest
+{
+    use GetClientAdapterTrait;
+}
