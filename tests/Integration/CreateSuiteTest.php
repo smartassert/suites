cb04bf9ddbdcb475eb9182d080cc409ae3a04253
Jon Cram <webignition@gmail.com>
2022-04-25 16:06:04 +0100

    Create integration tests (#78)

---
+++ tests/Integration/CreateSuiteTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use App\Tests\Application\AbstractCreateSuiteTest;
+
+class CreateSuiteTest extends AbstractCreateSuiteTest
+{
+    use GetClientAdapterTrait;
+}
