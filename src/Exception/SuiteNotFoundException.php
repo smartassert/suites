cd91a531d9302ab68c56cdd7ac10578a6de5546c
Jon Cram <webignition@gmail.com>
2022-09-14 15:00:45 +0100

    SuiteNotFoundException::suiteId is read-only

--- src/Exception/SuiteNotFoundException.php
+++ src/Exception/SuiteNotFoundException.php
@@ -8,7 +8,7 @@ class SuiteNotFoundException extends \Exception
 {
     public const MESSAGE = 'Suite "%s" not found';
 
-    public function __construct(private string $suiteId)
+    public function __construct(private readonly string $suiteId)
     {
         parent::__construct(sprintf(self::MESSAGE, $suiteId));
     }
